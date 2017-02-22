93fee54c401b8568c8c665c2b0a06e3fab4dfe43
Rei <chloerei@gmail.com>
2017-02-22 23:48:57 +0800

    update export script, fix code lange, list, table

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM ubuntu:16.04
 
+ENV LC_ALL C.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US.UTF-8
+
 RUN apt-get update && \
-    apt-get -y install pandoc ruby asciidoctor
+    apt-get -y install pandoc ruby ruby-dev build-essential zlib1g-dev
+
+RUN gem install asciidoctor nokogiri
 
 RUN mkdir /app
 WORKDIR /app
--- export.rb
+++ export.rb
@@ -1,8 +1,10 @@
 require 'fileutils'
+require 'nokogiri'
 
-FileUtils.rm_r 'export'
 FileUtils.mkdir_p 'export/docbook'
 FileUtils.mkdir_p 'export/md'
+FileUtils.rm Dir['export/docbook/*']
+FileUtils.rm Dir['export/md/*']
 
 Dir['manuscript/*.adoc'].each do |adoc|
   `asciidoctor -D export/docbook -b docbook5 #{adoc}`
@@ -10,4 +12,29 @@ end
 
 Dir['export/docbook/*.xml'].each do |docbook|
   `pandoc -f docbook -t markdown_github #{docbook} -o export/md/#{File.basename docbook, '.xml'}.md`
+end
+
+Dir['export/md/*.md'].each do |file|
+  content = File.read file
+
+  content.gsub! /^-   /, '- '
+  content.gsub! /^``` (.*)$/, '```\1'
+
+  content.gsub!(/<table>.+?<\/table>/m) do |html|
+    doc = Nokogiri::HTML(html)
+    lines = []
+
+    if head = doc.at_css('thead tr')
+      lines << ['|', head.css('th').map{|th| " #{th.content} "}.join('|'), '|'].join
+      lines << '-' * lines.first.length
+    end
+
+    doc.css('tbody tr').each do |tr|
+      lines << ['|', tr.css('td').map{|td| " #{td.content} "}.join('|'), '|'].join
+    end
+
+    lines.join("\n")
+  end
+
+  File.open(file, 'w') { |f| f.write content }
 end
